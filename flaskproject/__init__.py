60ea571bd5fb7cb8da4e6472a37ecf00ac762c62
Asif <ama85bd@gmail.com>
2020-09-14 17:40:52 +0600

    final code

--- flaskproject/__init__.py
+++ flaskproject/__init__.py
@@ -30,8 +30,10 @@ def create_app(config_class=Config):
     from flaskproject.users.routes import users
     from flaskproject.posts.routes import posts
     from flaskproject.main.routes import main
+    from flaskproject.errors.handlers import errors
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
